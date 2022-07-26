7dabfca73fb70d95a033654c4551d85a26764671
EdwinTJ <edwin16x@gmail.com>
2022-07-26 01:27:57 -0600

    Add Pug library, and Customer ticekts

--- utilities/SendEmail.js
+++ utilities/SendEmail.js
@@ -1,5 +1,4 @@
 import nodemailer from "nodemailer";
-import { viewAdmin } from "../views/ticketAdmin";
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
@@ -42,11 +41,20 @@ export const sendEmailToAdmin = async (dataForm) => {
   let info = await transporter.sendMail({
     from: ` "Insurance:" <edwininsurancex@outlook.com>`, // sender address
     to: "edwin16x@gmail.com", // list of receivers
-    subject: "Example", // Subject line
-    text: "Hello world?", // plain text body
-    html: viewAdmin, // html body
+    subject: "New Message Received", // Subject line
+    html: `
+    <h1>You Have Received a New Message</h1>
+    <p>The message has come from ${email}</p>
+    <h3>Name of the person:</h3>
+    <!-- Indent his p -->
+    <p>Hello my name is ${fullName}</p>
+    <h3>The message send by ${fullName} is :</h3>
+    <!-- Indent this p -->
+    <p>${message}</p>
+    <h3>My last insurance company was:</h3>
+    <p>${coverage}</p>
+  `, // html body
   });
-
   console.log("Message sent: %s", info.messageId);
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 };
--- views/ticketAdmin.js
+++ views/ticketAdmin.js
@@ -1,8 +1,15 @@
-export const viewAdmin = () => {
+export const viewAdmin = (config) => {
   console.log("working view");
-  `<h1>New Message from ${config.fullName}</h1>
-  <p>email : ${config.email}, coverage: ${config.coverage}</p>
-  <h3>Message:</h3>
+  `
+  <h1>You Have Received a New Message</h1>
+  <p>The message has come from ${config.email}</p>
+  <h3>Name of the person</h3>
+  <!-- Indent his p -->
+  <p>Hello my name is ${config.fullName}</p>
+  <h3>The message send by ${config.fullName} is :</h3>
+  <!-- Indent this p -->
   <p>${config.message}</p>
+  <h3>My last insurance company was:</h3>
+  <p>${config.coverage}</p>
 `;
 };
