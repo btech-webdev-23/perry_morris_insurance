00f705ab23b849b5cdfa7331c2cee5519f1666e5
EdwinTJ <edwin16x@gmail.com>
2022-08-10 20:55:54 -0600

    Add Template to email

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
  * Required External Modules
  */
-import path from "path";
-import express, { request } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import {
   sendEmailToAdmin,
   sendEmailToCostumer,
 } from "./utilities/SendEmail.js";
+import path from "path";
 import { fileURLToPath } from "url";
 
 /**
@@ -17,16 +17,12 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = process.env.PORT || "8000";
 
+const __filename = fileURLToPath(import.meta.url);
+export const rootPath = path.dirname(__filename);
+
 /**
  *  App Configuration
  */
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "public")));
-console.log("__dirname", path.join(__dirname, "views"));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -37,32 +33,11 @@ app.use(bodyParser.raw());
  */
 
 app.post("/email", (req, res) => {
-  sendEmailToAdmin(req.body);
+  sendEmailToCostumer(req.body);
 
   res.send();
 });
 
-// Render Ticket
-app.get("/", (req, res) => {
-  res.render("ticketAdmin", {
-    name: "Edwin Silvestre",
-    email: "edwin16x@gmail.com",
-    coverage: "State Farm",
-    message: "I want info about this",
-  });
-});
-
-app.get("/emailToCustomer", (req, res) => {
-  console.log(
-    "viewPug",
-    res.render("ticketCustomer", {
-      name: "Edwin Silvestre",
-      email: "edwin16x@gmail.com",
-      coverage: "State Farm",
-      message: "I want info about this",
-    })
-  );
-});
 /**
  * Server Activation
  */
